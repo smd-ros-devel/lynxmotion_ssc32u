3a735065696dc28dfa2dec750c43f3ee00bf50d4
Tony Baltovski <tony.baltovski@gmail.com>
2015-01-20 15:38:28 -0500

    Changed the scope of the scale.

--- src/ssc32_driver.cpp
+++ src/ssc32_driver.cpp
@@ -24,6 +24,7 @@ SSC32Driver::SSC32Driver( ros::NodeHandle &nh ) :
 
 	if ( range_scale > 1 || range_scale <= 0 )
 		range_scale = 1.0;
+	scale = range_scale * 2000.0 / M_PI;
 
 	// Parse joints ros param
 	XmlRpc::XmlRpcValue joints_list;
@@ -191,7 +192,6 @@ SSC32Driver::~SSC32Driver( )
 bool SSC32Driver::init( )
 {
 	SSC32::ServoCommand *cmd;
-	const double scale = range_scale * 2000.0 / M_PI;
 	bool success = true;
 
 	// Initialize each controller
@@ -362,7 +362,6 @@ void SSC32Driver::publishJointStates( )
 					pw = 3000 - pw;
 
 				//ROS_DEBUG( "Pulse width for joint [%s] is %d", controllers[i]->joints[j]->name.c_str( ), pw );
-				const double scale = range_scale * 2000.0 / M_PI;
 				//double angle = M_PI_2 * ( ( double )pw - controllers[i]->joints[j]->properties.default_angle ) / 1000.0;
 				double angle = ( ( double ) pw - 1500.0 ) / scale + controllers[i]->joints[j]->properties.offset_angle;
 
@@ -415,7 +414,6 @@ void SSC32Driver::jointCallback( const ros::MessageEvent<trajectory_msgs::JointT
 
 	bool invalid = false;
 
-	const double scale = range_scale * 2000.0 / M_PI;
 
 	for( unsigned int i = 0; i < msg->joint_names.size( ) && !invalid; i++ )
 	{
