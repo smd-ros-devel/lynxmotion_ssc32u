ed5ff9690d759523dab22be38d9ddad48c02725d
mattrichard <matthew.w.richard@gmail.com>
2015-02-08 16:27:46 -0700

    Port and baud can now be given as command line arugments. Also added some error checking.

--- src/test_ssc32.cpp
+++ src/test_ssc32.cpp
@@ -3,12 +3,47 @@
 #include <cstdlib>
 #include "lynxmotion_ssc32/ssc32.h"
 
+bool is_int( char *str )
+{
+	int i = 0;
+	int len = strlen( str );
+
+	if( len <= 0 )
+		return false;
+
+	if( str[i] != '-' && ( str[i] < '0' || str[i] > '9' ) )
+		return false;
+
+	i++;
+	while( i < len )
+	{
+		if( str[i] < '0' || str[i] > '9' )
+			return false;
+
+		i++;
+	}
+
+	return true;
+}
+
+bool check_arg_is_int( char *str )
+{
+	if( !is_int( str ) )
+	{
+		std::cout << "Invalid argument '" << str << "'" << std::endl;
+		return false;
+	}
+
+	return true;
+}
+
 int main( int argc, char **argv )
 {
-	std::string port = "/dev/ttyUSB0";
-	int baud = 115200;
+	std::string port;
+	int baud;
 	lynxmotion_ssc32::SSC32 ssc32_device;
 	std::string version;
+	int i = 1;
 
 	if( argc < 2 )
 	{
@@ -17,9 +52,53 @@ int main( int argc, char **argv )
 		return 1;
 	}
 
-	if( strcmp( argv[1], "--help" ) == 0 || strcmp( argv[1], "-h" ) == 0 )
+	// Check if port was provided as an argument
+	if( strcmp( argv[i], "-p" ) == 0 )
 	{
-		std::cout << "Usage: " << argv[0] << " COMMAND" << std::endl;
+		if( argc < 4)
+		{
+			std::cout << argv[0] << ": You must specify a command" << std::endl;
+			std::cout << "Try `" << argv[0] << " --help' for more information." << std::endl;
+			return 1;
+		}
+
+		i++;
+		port = argv[i];
+		i++;
+	}
+	else
+	{
+		std::cout << "port not specified -- defaulting to /dev/ttyUSB0" << std::endl;
+		port = "/dev/ttyUSB0";
+	}
+
+	// Check if baud was provided as an argument
+	if( strcmp( argv[i], "-b" ) == 0 )
+	{
+		if( i + 2 >= argc )
+		{
+			std::cout << argv[0] << ": You must specify a command" << std::endl;
+			std::cout << "Try `" << argv[0] << " --help' for more information." << std::endl;
+			return 1;
+		}
+
+		i++;
+		if( !check_arg_is_int( argv[i] ) )
+			return 1;
+		baud = atoi( argv[i] );
+		i++;
+	}
+	else
+	{
+		std::cout << "baud not specified -- defaulting to 115200" << std::endl;
+		baud = 115200;
+	}
+
+	if( strcmp( argv[i], "--help" ) == 0 || strcmp( argv[i], "-h" ) == 0 )
+	{
+		std::cout << "Usage: " << argv[0] << " [-p PORT] [-b BAUD] COMMAND" << std::endl;
+		std::cout << std::endl;
+		std::cout << "If provided, PORT and BAUD must be given in the specified order above." << std::endl;
 		std::cout << std::endl;
 		std::cout << "Possible commands:" << std::endl;
 		std::cout << "  -a                     Read inputs as analog." << std::endl
@@ -28,10 +107,10 @@ int main( int argc, char **argv )
 			  << "  -i                     Read inputs as digital." << std::endl
 			  << "  -m CH PW [S] [T]       Move a servo on channel CH to the given pulse width PW." << std::endl
 			  << "  -o CH OFFSET           Offsets channel CH by OFFSET amount." << std::endl
-			  << "  -s                     Checks if any servos are currently moving" << std::endl
+			  << "  -s                     Checks if any servos are currently moving." << std::endl
 			  << "  -v                     Get the software version." << std::endl;
 	}
-	else if( strcmp( argv[1], "-v" ) == 0 )
+	else if( strcmp( argv[i], "-v" ) == 0 )
 	{
 		if( !ssc32_device.open_port( port.c_str( ), baud ) )
 			return -1;
@@ -42,7 +121,7 @@ int main( int argc, char **argv )
 
 		std::cout << "SSC32 Software Version: " << version << std::endl;
 	}
-	else if( strcmp( argv[1], "-c" ) == 0 )
+	else if( strcmp( argv[i], "-c" ) == 0 )
 	{
 		if( !ssc32_device.open_port( port.c_str( ), baud ) )
 			return -1;
@@ -53,27 +132,40 @@ int main( int argc, char **argv )
 			return -1;
 
 		std::cout << "Done" << std::endl;
-			
 	}
-	else if( strcmp( argv[1], "-m" ) == 0 )
+	else if( strcmp( argv[i], "-m" ) == 0 )
 	{
-		if( argc < 4 )
+		if( i + 2 > argc )
 		{
-			std::cout << "Usage: " << argv[0] << " -m CHANNEL PULSE_WIDTH [SPEED] [TIME]" << std::endl;
+			std::cout << "Usage: " << argv[0] << " [-p PORT] [-b BAUD] -m CHANNEL PULSE_WIDTH [SPEED] [TIME]" << std::endl;
 			return 1;
 		}
 
 		lynxmotion_ssc32::SSC32::ServoCommand cmd;
-		cmd.ch = atoi( argv[2] );
-		cmd.pw = atoi( argv[3] );
+
+		if( !check_arg_is_int( argv[i + 1] ) )
+			return 1;
+		cmd.ch = atoi( argv[i + 1] );
+
+		if( !check_arg_is_int( argv[i + 2] ) )
+			return 1;
+		cmd.pw = atoi( argv[i + 2] );
 
 		int time = -1;
 
-		if( argc > 4 )
-			cmd.spd = atoi( argv[4] );
+		if( i + 3 < argc )
+		{
+			if( !check_arg_is_int( argv[i + 3] ) )
+				return 1;
+			cmd.spd = atoi( argv[i + 3] );
+		}
 
-		if( argc > 5 )
-			time = atoi( argv[5] );
+		if( i + 4 < argc )
+		{
+			if( !check_arg_is_int( argv[i + 4] ) )
+				return 1;
+			time = atoi( argv[i + 4] );
+		}
 
 		if( !ssc32_device.open_port( port.c_str( ), baud ) )
 			return 1;
@@ -83,16 +175,21 @@ int main( int argc, char **argv )
 		if( !ssc32_device.move_servo( cmd, time ) )
 			return 1;
 	}
-	else if( strcmp( argv[1], "-o" ) == 0 || strcmp( argv[1], "--offset" ) == 0 )
+	else if( strcmp( argv[i], "-o" ) == 0 || strcmp( argv[i], "--offset" ) == 0 )
 	{
-		if( argc != 4 )
+		if( i + 2 >= argc )
 		{
-			std::cout << "Usage: " << argv[0] << " -o CHANNEL OFFSET" << std::endl;
+			std::cout << "Usage: " << argv[0] << " [-p PORT] [-b BAUD] -o CHANNEL OFFSET" << std::endl;
 			return 1;
 		}
 
-		int ch = atoi( argv[2] );
-		int offset = atoi( argv[3] );
+		if( !check_arg_is_int( argv[i + 1] ) )
+			return 1;
+		int ch = atoi( argv[i + 1] );
+
+		if( !check_arg_is_int( argv[i + 2] ) )
+			return 1;
+		int offset = atoi( argv[i + 2] );
 
 		if( !ssc32_device.open_port( port.c_str( ), baud ) )
 			return 1;
@@ -102,7 +199,7 @@ int main( int argc, char **argv )
 		if( !ssc32_device.pulse_offset( ch, offset ) )
 			return 1;
 	}
-	else if( strcmp( argv[1], "-s" ) == 0 || strcmp( argv[1], "--status" ) == 0 )
+	else if( strcmp( argv[i], "-s" ) == 0 || strcmp( argv[i], "--status" ) == 0 )
 	{
 		if( !ssc32_device.open_port( port.c_str( ), baud ) )
 			return 1;
@@ -112,20 +209,22 @@ int main( int argc, char **argv )
 		else
 			std::cout << "No servos are currently moving." << std::endl;
 	}
-	else if( strcmp( argv[1], "-d" ) == 0 )
+	else if( strcmp( argv[i], "-d" ) == 0 )
 	{
-		if( argc != 4 )
+		if( i + 2 >= argc )
 		{
-			std::cout << "Usage: " << argv[0] << " -d CHANNEL LEVEL" << std::endl;
+			std::cout << "Usage: " << argv[0] << " [-p PORT] [-b BAUD] -d CHANNEL LEVEL" << std::endl;
 			return 1;
 		}
 
-		int ch = atoi( argv[2] );
-		lynxmotion_ssc32::SSC32::LogicLevel level;
+		if( !check_arg_is_int( argv[i + 1] ) )
+			return 1;
+		int ch = atoi( argv[i + 1] );
 
-		if( strcmp( argv[3], "0" ) == 0 )
+		lynxmotion_ssc32::SSC32::LogicLevel level;
+		if( strcmp( argv[i + 2], "0" ) == 0 )
 			level = lynxmotion_ssc32::SSC32::Low;
-		else if( strcmp( argv[3], "1" ) == 0 )
+		else if( strcmp( argv[i + 2], "1" ) == 0 )
 			level = lynxmotion_ssc32::SSC32::High;
 		else
 		{
@@ -139,7 +238,7 @@ int main( int argc, char **argv )
 		if( !ssc32_device.discrete_output( ch, level ) )
 			return 1;
 	}
-	else if( strcmp( argv[1], "-a" ) == 0 )
+	else if( strcmp( argv[i], "-a" ) == 0 )
 	{
 		lynxmotion_ssc32::SSC32::Inputs inputs[4];
 		float data[4];
@@ -155,10 +254,10 @@ int main( int argc, char **argv )
 		if( !ssc32_device.read_analog_inputs( inputs, data, 4 ) )
 			return 1;
 
-		for( int i = 0; i < 4; i++ )
-			std::cout << "Pin " << ( ( char )i + 'A' ) << ": " << data[i] << std::endl;
+		for( int j = 0; j < 4; j++ )
+			std::cout << "Pin " << ( ( char )j + 'A' ) << ": " << data[j] << std::endl;
 	}
-	else if( strcmp( argv[1], "-i" ) == 0 )
+	else if( strcmp( argv[i], "-i" ) == 0 )
 	{
 		lynxmotion_ssc32::SSC32::Inputs inputs[4];
 		unsigned int data[4];
@@ -174,12 +273,12 @@ int main( int argc, char **argv )
 		if( !ssc32_device.read_digital_inputs( inputs, data, 4 ) )
 			return 1;
 
-		for( int i = 0; i < 4; i++ )
-			std::cout << "Pin " << ( ( char )i + 'A' ) << ": " << data[i] << std::endl;
+		for( int j = 0; j < 4; j++ )
+			std::cout << "Pin " << ( ( char )j + 'A' ) << ": " << data[j] << std::endl;
 	}
 	else
 	{
-		std::cout << argv[0] << ": invalid option -- '" << argv[1]+1 << "'" << std::endl;
+		std::cout << argv[0] << ": invalid option -- '" << argv[i] << "'" << std::endl;
 		std::cout << "Try `" << argv[0] << " --help' for more information." << std::endl;
                 return 1;
 	}
