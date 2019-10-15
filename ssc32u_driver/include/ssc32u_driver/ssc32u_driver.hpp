9500351e14fdc11443819aea0cb51cc5fa052dd0
mattrichard <mattric@gmail.com>
2019-10-15 08:38:30 -0700

    Add initial subscribers/services

--- ssc32u_driver/include/ssc32u_driver/ssc32u_driver.hpp
+++ ssc32u_driver/include/ssc32u_driver/ssc32u_driver.hpp
@@ -4,6 +4,9 @@
 #include "rclcpp/rclcpp.hpp"
 #include "rclcpp_lifecycle/lifecycle_node.hpp"
 #include "ssc32.hpp"
+#include "ssc32u_msgs/msg/servo_command_group.hpp"
+#include "ssc32u_msgs/msg/discrete_output.hpp"
+#include "ssc32u_msgs/srv/query_pulse_width.hpp"
 
 namespace ssc32u_driver
 {
@@ -30,6 +33,16 @@ public:
 
 private:
   SSC32 ssc32_;
+  rclcpp::Subscription<ssc32u_msgs::msg::ServoCommandGroup>::SharedPtr servo_command_sub_;
+  rclcpp::Subscription<ssc32u_msgs::msg::DiscreteOutput>::SharedPtr discrete_output_sub_;
+  rclcpp::Service<ssc32u_msgs::srv::QueryPulseWidth>::SharedPtr query_pw_srv_;
+
+  void command_received(const ssc32u_msgs::msg::ServoCommandGroup::SharedPtr msg);
+  void discrete_output(const ssc32u_msgs::msg::DiscreteOutput::SharedPtr msg);
+
+  void query_pulse_width(const std::shared_ptr<rmw_request_id_t> request_header,
+    const std::shared_ptr<ssc32u_msgs::srv::QueryPulseWidth::Request> request,
+    std::shared_ptr<ssc32u_msgs::srv::QueryPulseWidth::Response> response);
 };
 
 }  // namespace ssc32u_driver
--- ssc32u_driver/src/ssc32u_driver.cpp
+++ ssc32u_driver/src/ssc32u_driver.cpp
@@ -9,6 +9,8 @@
 
 using namespace std::chrono_literals;
 using std::placeholders::_1;
+using std::placeholders::_2;
+using std::placeholders::_3;
 
 namespace ssc32u_driver
 {
@@ -23,12 +25,32 @@ SSC32UDriver::SSC32UDriver(const rclcpp::NodeOptions & options)
 rclcpp_lifecycle::node_interfaces::LifecycleNodeInterface::CallbackReturn
 SSC32UDriver::on_configure(const rclcpp_lifecycle::State &)
 {
+  std::string port;
+  int baud;
+
+  this->get_parameter("port", port);
+  this->get_parameter("baud", baud);
+
+  if (!this->ssc32_.open_port(port.c_str(), baud)) {
+    RCLCPP_ERROR(this->get_logger(), "Unable to initialize the SSC32");
+    return rclcpp_lifecycle::node_interfaces::LifecycleNodeInterface::CallbackReturn::ERROR;
+  }
+
   return rclcpp_lifecycle::node_interfaces::LifecycleNodeInterface::CallbackReturn::SUCCESS;
 }
 
 rclcpp_lifecycle::node_interfaces::LifecycleNodeInterface::CallbackReturn
 SSC32UDriver::on_activate(const rclcpp_lifecycle::State &)
 {
+  this->servo_command_sub_ = this->create_subscription<ssc32u_msgs::msg::ServoCommandGroup>(
+    "servo_cmd", 1, std::bind(&SSC32UDriver::command_received, this, _1));
+
+  this->discrete_output_sub_ = this->create_subscription<ssc32u_msgs::msg::DiscreteOutput>(
+    "discrete_output", 1, std::bind(&SSC32UDriver::discrete_output, this, _1));
+
+  this->query_pw_srv_ = this->create_service<ssc32u_msgs::srv::QueryPulseWidth>(
+    "query_pulse_width", std::bind(&SSC32UDriver::query_pulse_width, this, _1, _2, _3));
+
   return rclcpp_lifecycle::node_interfaces::LifecycleNodeInterface::CallbackReturn::SUCCESS;
 }
 
@@ -47,7 +69,36 @@ SSC32UDriver::on_cleanup(const rclcpp_lifecycle::State &)
 rclcpp_lifecycle::node_interfaces::LifecycleNodeInterface::CallbackReturn
 SSC32UDriver::on_shutdown(const rclcpp_lifecycle::State &)
 {
+  this->ssc32_.close_port();
+
   return rclcpp_lifecycle::node_interfaces::LifecycleNodeInterface::CallbackReturn::SUCCESS;
+}
+
+void SSC32UDriver::command_received(const ssc32u_msgs::msg::ServoCommandGroup::SharedPtr msg)
+{
+  for (auto &command : msg->commands) {
+    ssc32u_driver::SSC32::ServoCommand cmd;
+    cmd.ch = command.channel;
+    cmd.pw = command.pw;
+
+    this->ssc32_.move_servo(cmd);
+  }
+}
+
+void SSC32UDriver::discrete_output(const ssc32u_msgs::msg::DiscreteOutput::SharedPtr msg)
+{
+  this->ssc32_.discrete_output(msg->channel, msg->output ? SSC32::High : SSC32::Low);
+}
+
+void SSC32UDriver::query_pulse_width(const std::shared_ptr<rmw_request_id_t>,
+  const std::shared_ptr<ssc32u_msgs::srv::QueryPulseWidth::Request> request,
+  std::shared_ptr<ssc32u_msgs::srv::QueryPulseWidth::Response> response)
+{
+  for (auto & channel : request->channels) {
+    int pw = this->ssc32_.query_pulse_width(channel);
+
+    response->pulse_width.push_back(pw);
+  }
 }
 
 }  // namespace ssc32u_driver
